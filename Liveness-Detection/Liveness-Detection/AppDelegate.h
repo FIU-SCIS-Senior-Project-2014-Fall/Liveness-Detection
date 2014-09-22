db9406aa5afa1c2704dcf7b5af3d19c0f30d04f9
Christopher Choitz <cchoi003@fiu.edu>
2014-09-22 12:20:05 -0400

    Initial commit of code

---
+++ Liveness-Detection/Liveness-Detection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Liveness-Detection
+//
+//  Created by Christopher Choitz on 9/22/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Liveness-Detection/Liveness-Detection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Liveness-Detection
+//
+//  Created by Christopher Choitz on 9/22/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
